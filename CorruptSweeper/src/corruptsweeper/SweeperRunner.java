0f88299ba4487e97a31774c612ba4f0a43f38a52
Jack <jackpergantis@gmail.com>
2016-01-16 08:38:48 -0600

    SweeperRunner now manages automatic map updates! In other words, it "works" but still requires fine tuning for timing and accuracy.

--- CorruptSweeper/src/corruptsweeper/SweeperRunner.java
+++ CorruptSweeper/src/corruptsweeper/SweeperRunner.java
@@ -6,12 +6,62 @@
 
 package corruptsweeper;
 
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+import org.sikuli.api.*;
+import org.sikuli.api.event.*;
+
 public class SweeperRunner {	
+	
+	private SweeperFrame sf;
+	
+	// Variables used to capture, update, and store the map and its location
+	private MapCapturer mapCapturer = new MapCapturer();
+	private MapFinder mapFinder = new MapFinder();
+	private Target mapX;
+	private ScreenRegion mapXListenerRegion;
 	/**
 	 * Main method to run the program 
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		new SweeperFrame();
+		new SweeperRunner();
 	}
+	
+	public SweeperRunner() {
+		sf = new SweeperFrame();		
+		try {
+			mapX = new ImageTarget(ImageIO.read(getClass().getResource("/resources/MapX.png")));
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		mapX.setMinScore(1.00);
+		mapXListenerRegion = new DesktopScreenRegion(mapFinder.findMapX().x, mapFinder.findMapX().y, 16, 16); //TODO - use variables
+		mapXListenerRegion.addTargetEventListener(mapX, listener);
+	}
+	
+	/**
+	 * Creates a TargetEventListener to automatically update the map when the "x" is detected in the location designated by sf.getMapLoc()
+	 */
+	TargetEventListener listener = new TargetEventListener() {       				
+		@Override
+		public void targetAppeared(TargetEvent event) {
+			System.out.println("Target found! Attempting to update...");
+			sf.updateMap(new ImageIcon(mapCapturer.getMap(sf.getMapLoc())));
+		}
+
+		@Override
+		public void targetMoved(TargetEvent event) {
+			// Nothing required, implementation required by interface
+		}
+
+		@Override
+		public void targetVanished(TargetEvent event) {
+			// Nothing required, implementation required by interface
+		}
+	};
 }
