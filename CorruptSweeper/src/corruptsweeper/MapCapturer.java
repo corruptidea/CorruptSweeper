81dda8aeac9ed8c83fecd7d2a26020698f3c9885
Jack <jackpergantis@gmail.com>
2016-01-16 05:33:03 -0600

    - Adjusted size of image to capture - No map found now throws NullPointerException

--- CorruptSweeper/src/corruptsweeper/MapCapturer.java
+++ CorruptSweeper/src/corruptsweeper/MapCapturer.java
@@ -12,27 +12,23 @@ import java.awt.AWTException;
 
 public class MapCapturer {
 	
-	private MapFinder mf = new MapFinder(); 
-	private final Point MAP_TOP_LEFT = mf.findMap();
-	
-	private final int MAP_LENGTH = 341;
-	private final int MAP_WIDTH = 322;
-	private final Dimension MAP_SIZE = new Dimension(MAP_LENGTH, MAP_WIDTH);
+	private final int MAP_LENGTH = 310;
+	private final int MAP_WIDTH = 318;
+	private final Dimension MAP_SIZE = new Dimension(MAP_WIDTH, MAP_LENGTH);
 	
 	// BufferedImage of the map to be returned
 	private BufferedImage mapImg;
 	
 	/**
-	 * Generates an image of the map to display 
+	 * Capture an image of the map to display 
 	 * TODO - Only capture if is image of map
-	 * @param p the top left corner of the map
-	 * @param d the size of the map
+	 * @param loc the top left corner of the map
 	 * @return the BufferedImage of the map
 	 */
-	public BufferedImage getMap() {
+	public BufferedImage getMap(Point loc) {
 		try {
 			Robot r = new Robot();
-			mapImg = r.createScreenCapture(new Rectangle(MAP_TOP_LEFT, MAP_SIZE));
+			mapImg = r.createScreenCapture(new Rectangle(loc, MAP_SIZE));
 		}
 		// Should not occur - only if user attempts to run in CLI environment
 		catch (AWTException e) {
