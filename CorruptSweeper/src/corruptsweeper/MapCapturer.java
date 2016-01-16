0edde65285e8a1036e894de9dbd6bd1d7d85435d
Jack <jackpergantis@gmail.com>
2016-01-16 08:43:13 -0600

    Added header

--- CorruptSweeper/src/corruptsweeper/MapCapturer.java
+++ CorruptSweeper/src/corruptsweeper/MapCapturer.java
@@ -1,3 +1,9 @@
+/**
+ * The MapCapturer class contains a function to capture the map on the screen for display
+ * 
+ * @author Jack Pergantis 
+ */
+
 package corruptsweeper;
 
 // Shape classes
@@ -15,25 +21,23 @@ public class MapCapturer {
 	private final int MAP_LENGTH = 310;
 	private final int MAP_WIDTH = 318;
 	private final Dimension MAP_SIZE = new Dimension(MAP_WIDTH, MAP_LENGTH);
+
 	
 	// BufferedImage of the map to be returned
 	private BufferedImage mapImg;
 	
 	/**
 	 * Capture an image of the map to display 
-	 * TODO - Only capture if is image of map
 	 * @param loc the top left corner of the map
 	 * @return the BufferedImage of the map
 	 */
-	public BufferedImage getMap(Point loc) {
+	public BufferedImage getMap(Point loc) {	
 		try {
 			Robot r = new Robot();
 			mapImg = r.createScreenCapture(new Rectangle(loc, MAP_SIZE));
 		}
-		// Should not occur - only if user attempts to run in CLI environment
 		catch (AWTException e) {
-			System.out.println("GraphicsEnvironment is Headless");
-			e.printStackTrace();
+		// Should only if user attempts to run in CLI environment
 		}		
 		return mapImg;
 	}	
