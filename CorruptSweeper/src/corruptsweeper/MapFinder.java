e66f712ff2afcf7a5674c19b71b2e5fcdcbc8482
Jack <jackpergantis@gmail.com>
2016-01-16 05:33:42 -0600

    No map found now throws NullPointerException

--- CorruptSweeper/src/corruptsweeper/MapFinder.java
+++ CorruptSweeper/src/corruptsweeper/MapFinder.java
@@ -13,19 +13,20 @@ import org.sikuli.api.*;
 
 public class MapFinder {
 	
-	private final int deltaX = 313; // Difference between left bound of X and left bound of map
-	private final int deltaY = 11; // Difference between top bound of X and top bound of map
+	private final int deltaX = 300; // Difference between left bound of X and left bound of map
+	private final int deltaY = 3; // Difference between top bound of X and top bound of map
 	
 	/**
 	 * @return a Point representing the top left corner of the map
 	 */
-	public Point findMap() {
+	public Point findMap() throws NullPointerException {
 		
 		ScreenRegion fullscreen = new DesktopScreenRegion(); // Currently works on default monitor only!
 		Target imageTarget = null;
 		try {
 			imageTarget = new ImageTarget(ImageIO.read(getClass().getResource("/resources/MapX.png")));
-		} catch (IOException e) {
+		} 
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 		Point mapXLoc = fullscreen.find(imageTarget).getBounds().getLocation();		
