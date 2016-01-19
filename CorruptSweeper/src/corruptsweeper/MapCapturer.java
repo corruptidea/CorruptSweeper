7cc5bb09f308b95f35cd554023a093d3b9531cdf
Jack <jackpergantis@gmail.com>
2016-01-19 08:03:39 -0600

    Now uses Sikuli's DesktopScreenRegion.capture() instead of java.util.Robot to capture map for speed purposes

--- CorruptSweeper/src/corruptsweeper/MapCapturer.java
+++ CorruptSweeper/src/corruptsweeper/MapCapturer.java
@@ -8,20 +8,17 @@ package corruptsweeper;
 
 // Shape classes
 import java.awt.Point;
-import java.awt.Dimension;
-import java.awt.Rectangle;
 
 // Image capture classes
-import java.awt.Robot;
 import java.awt.image.BufferedImage;
-import java.awt.AWTException;
+
+// Sikuli
+import org.sikuli.api.*;
 
 public class MapCapturer {
 	
 	private final int MAP_LENGTH = 310;
 	private final int MAP_WIDTH = 318;
-	private final Dimension MAP_SIZE = new Dimension(MAP_WIDTH, MAP_LENGTH);
-
 	
 	// BufferedImage of the map to be returned
 	private BufferedImage mapImg;
@@ -32,13 +29,7 @@ public class MapCapturer {
 	 * @return the BufferedImage of the map
 	 */
 	public BufferedImage getMap(Point loc) {	
-		try {
-			Robot r = new Robot();
-			mapImg = r.createScreenCapture(new Rectangle(loc, MAP_SIZE));
-		}
-		catch (AWTException e) {
-		// Should only if user attempts to run in CLI environment
-		}		
+		mapImg = new DesktopScreenRegion(loc.x, loc.y, MAP_WIDTH, MAP_LENGTH).capture();
 		return mapImg;
 	}	
 }
