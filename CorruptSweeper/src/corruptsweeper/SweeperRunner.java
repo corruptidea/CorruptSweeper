f6d4e5f682c217766358e58b03c677469f529f91
Jack <jackpergantis@gmail.com>
2016-01-19 08:09:16 -0600

    Returned responsibility for capturing map to SweeperFrame - see commit dcc5ae51f2240eb21482ad25631f5128868f1f70 for details

--- CorruptSweeper/src/corruptsweeper/SweeperRunner.java
+++ CorruptSweeper/src/corruptsweeper/SweeperRunner.java
@@ -6,46 +6,20 @@
 
 package corruptsweeper; 
 
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import org.sikuli.api.*;
+import javax.swing.SwingUtilities;
 
 public class SweeperRunner {	
-	
-	private SweeperFrame sf;
-	
-	// Variables used to capture, update, and store the map and its location
-	private MapCapturer mapCapturer = new MapCapturer();
-	private Target mapX; 
+
 	/**
 	 * Main method to run the program 
 	 * @param args the command line arguments
 	 * @throws InterruptedException 
 	 */
 	public static void main(String[] args) throws InterruptedException {
-		new SweeperRunner();		
-	}
-	
-	public SweeperRunner() throws InterruptedException { 
-		sf = new SweeperFrame();
-		while (sf.getMapXLoc() == null) {
-			Thread.sleep(1);
-		}
-		System.out.println("biggles");
-		try {
-			mapX = new ImageTarget(ImageIO.read(getClass().getResource("/resources/MapX.png")));
-		} 
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		ScreenRegion searchable = new DesktopScreenRegion(sf.getMapXLoc().x, sf.getMapXLoc().y, 16, 16);
-		while (true) {
-			if (searchable.find(mapX) != null) {
-				sf.updateMap(new ImageIcon(mapCapturer.getMap(sf.getMapLoc())));
-				Thread.sleep(100);
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new SweeperFrame();
 			}
-		}
+		});	
 	}
 }
