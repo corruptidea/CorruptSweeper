0ba67c65182a0f3c4db74259b10043225b817ce2
Jack <jackpergantis@gmail.com>
2016-01-21 05:48:11 -0600

    Added BufferedImageComparator class

---
+++ CorruptSweeper/src/corruptsweeper/BufferedImageComparator.java
@@ -0,0 +1,28 @@
+package corruptsweeper;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferByte;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BufferedImageComparator implements Comparator<BufferedImage> {
+
+	@Override
+	public int compare(BufferedImage a, BufferedImage b) {
+		DataBuffer dbA = a.getRaster().getDataBuffer();
+		DataBuffer dbB = b.getRaster().getDataBuffer();
+		
+		DataBufferByte aDBAsDBByte = (DataBufferByte) dbA;
+		DataBufferByte bDBAsDBByte = (DataBufferByte) dbB;
+		
+		for (int bank = 0; bank < aDBAsDBByte.getNumBanks();) {
+			   byte[] actual = aDBAsDBByte.getData(bank);
+			   byte[] expected = bDBAsDBByte.getData(bank);
+			   
+			   return Arrays.equals(actual, expected) ? 1 : 0;
+		}
+		return 0;
+	}
+
+}
